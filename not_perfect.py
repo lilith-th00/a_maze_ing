e26565e46281b52744fc245cac5904789036c47d
lilith-th00 <massyliaaaa7@gmail.com>
2026-03-06 03:13:44 +0000

    Adjust wall removal logic in not_perfect method

--- not_perfect.py
+++ not_perfect.py
@@ -1,55 +1,63 @@
-'''  def not_perfect(self) -> None:
+
+ def not_perfect(self) -> None:
         """Introduce random wall removals to create loops
         and make the maze imperfect."""
         if self.perfect is False:
-            print("ayyy")
             
             unvisited: list[tuple[int, int]] = []
             visited: list[tuple[int, int]] = []
-            n_dir: str = ""
             dirs: list[str] = ['E', 'W', 'N', 'S']
             w: int = self.width
             h: int = self.height
-﻿
-            wall: int = int(h*w*0.2)
+
+            wall: int = int(h*w*0.15)
+
             for r in range(h):
                 for c in range(w):
-                    if self.cells[r][c].is_visited:
-                        visited.append((c, r))
-﻿
-            for r in range(h):
-                for c in range(w):
-                    if not self.cells[r][c].is_visited:
-                        unvisited.append((c, r))
-            
-            while wall != 0 and unvisited:
-                x: int
-                y: int
-﻿
-                x, y = random.choice((unvisited))
-                unvisited.remove((x, y))
-                
-                dict_42: dict[str, bool] = {}
-                for d in dirs:
-                    x_m, y_m, n_dir, m_dir= self.direction[d]
-                    n_x, n_y = x+m_x, y+m_y
-                    if (n_x, n_y) in visited:
-                        dict_42.update({n_dir: False})
-                    else:
-                        dict_42.update({n_dir: True})
-                
-                dirs = random.shuffle(dirs)
-                if self.cells[y][x].wall[dirs]:
                     found: bool = True
                     i: int = 0
                     while i < 4 and found:
-                        next_dir = dirs[i]
-                        if (dict_42[next_dir] is True
-                            and (x != 0 or d != 'W')
-                            and (x != w-1 or d != 'E')
-                            and (y != 0 or d != 'N')
-                            and (y != h-1 or d != 'S')):
-                            self.cells[y][x].walls[d] = False
-                            wall -= 1
+                        if not self.cells[r][c].walls[dirs[i]]:
                             found = False
                         i += 1
+                    if found:
+                        visited.append((c, r))
+                    else:
+                        unvisited.append((c, r))
+            
+            while wall > 0 and unvisited:
+                x: int
+                y: int
+                if not unvisited:
+                    break
+                x, y = random.choice((unvisited))
+                unvisited.remove((x, y))
+                print("pp")
+                dict_42: dict[str, bool] = {}
+                for d in dirs:
+                    m_x, m_y, n_dir, m_dir= self.direction[d]
+                    n_x, n_y = x + m_x, y + m_y
+                    if 0 <= n_x < w and 0 <= n_y < h:
+                        if (n_x, n_y) in visited:
+                            dict_42[n_dir] = False
+                        else:
+                            dict_42[n_dir] = True
+                    else:
+                        dict_42[n_dir] = False
+
+                random.shuffle(dirs)
+                found: bool = True
+                i: int = 0
+                while i < 4 and found:
+                    next_dir = dirs[i]
+                    m_x, m_y, n_dir, m_dir = self.direction[next_dir]
+                    n_x, n_y = x + m_x, y + m_y
+                    if 0 <= n_x < w and 0 <= n_y < h:
+                        if (self.cells[n_y][n_x].walls[m_dir]
+                                and dict_42[next_dir]):
+                                self.cells[y][x].walls[n_dir] = False
+                                self.cells[n_y][n_x].walls[m_dir] = False
+                                wall -= 1
+                                print("uu")
+                                found = False
+                    i += 1
